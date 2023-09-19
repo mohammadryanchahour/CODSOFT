9392f58c1ba762d4c9702607e1aa72f28d4df4f6
mohammadryanchahour <mohammad.ryan133@gmail.com>
2023-09-20 04:19:31 +0500

    Update

--- mern-blog-app/client/src/App.js
+++ mern-blog-app/client/src/App.js
@@ -6,16 +6,20 @@ import Layout from './Layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { UserContextProvider } from './UserContext';
 
 function App() {
   return (
-    <Routes>
+    <UserContextProvider>
+      <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={ <Home /> } />
         <Route path='/login' element={ <Login /> } />
         <Route path='/register' element={ <Register /> } />
       </Route>
     </Routes>
+    </UserContextProvider>
+    
   );
 }
 
